c5df20e78ec497167a5ae6f20ff50a45fa04a43b
ArkhieLau <38354687+ArkhieLau@users.noreply.github.com>
2018-04-18 23:38:23 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("Your Activity", {type: "WATCHING"});
+  bot.user.setActivity("Configur", {type: "WATCHING"});
 
 });
 
